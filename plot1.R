438bfd427cde1e6895beac82539e5298be6fec1c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:17:25 -0400

    Saving plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,6 @@
 
 library(ggplot2)
 library(scales)
+
+population<-ggplot(df, aes(df$population))+ geom_histogram(color="black", fill="white", bins = 50)+  scale_x_continuous("Population")+ scale_y_continuous("Count")
+population
