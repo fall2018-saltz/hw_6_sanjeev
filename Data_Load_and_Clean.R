bb2e30686f7eec90a7894972031b58d839f70205
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 13:14:38 -0400

    Saving Data_Load_and_Clean.R

--- Data_Load_and_Clean.R
+++ Data_Load_and_Clean.R
@@ -1,1 +1,15 @@
 
+clean<-function(){
+  data=read.csv("scprc_est_2017_18_pop_res.csv",stringsAsFactors = FALSE)
+  data<-data[,-c(1:4)]
+  colnames(data)<-c('statesName','population','popover18','percentover18')
+  #Deleting the unneeded rows!!
+  data<-data[-c(1,10,53),]
+  #View(arrests)
+  #nrow(data)
+  #nrow(df)
+  common<-intersect(names(arrests$statesName),names(data$STATE))
+  df<-merge(data,arrests,by.x='statesName',by.y = 'statesName')
+  }
+
+clean()
