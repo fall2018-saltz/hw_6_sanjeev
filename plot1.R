8c78ac228fc38121b287d6313a54426c60f99983
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:29:40 -0400

    Saving plot1.R

--- plot1.R
+++ plot1.R
@@ -12,3 +12,7 @@ murder
 popbox<-ggplot(df, aes(x="", y=df$population, color=population))+geom_boxplot()+scale_y_continuous(name="Population", labels = comma)+
   stat_summary(fun.ymin=median, fun.ymax=median, fun.y=median, geom="crossbar")
 popbox
+
+#Box plot for Murder Rate
+murbox<-ggplot(df, aes(x="", y=df$Murder, color=Murder))+geom_boxplot()+scale_y_continuous(name="Murder", labels = comma)+
+  stat_summary(fun.ymin=median, fun.ymax=median, fun.y=median, geom="crossbar")
