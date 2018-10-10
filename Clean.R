608bc5c2b2ee4e8880f1163ad4af723e6bfa46fe
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 13:36:33 -0400

    Saving Clean.R

--- Clean.R
+++ Clean.R
@@ -1,14 +1,10 @@
 
-clean<-function(){
-  data=read.csv("scprc_est_2017_18_pop_res.csv")
-  #Deleting unneeded columns
-  data<-data[,-c(1:4)]
-  #renaming columns
-  colnames(data)<-c('statesName','population','popover18','percentover18')
-  #Deleting the unneeded rows!!
-  data<-data[-c(1,10,53),]
-  rownames(data)<-as.vector(data[,"statesName"])
-  }
-  
-clean()
+data=read.csv("scprc_est_2017_18_pop_res.csv")
+#Deleting unneeded columns
+data<-data[,-c(1:4)]
+#renaming columns
+colnames(data)<-c('statesName','population','popover18','percentover18')
+#Deleting the unneeded rows!!
+data<-data[-c(1,10,53),]
+rownames(data)<-as.vector(data[,"statesName"])
 nrow(data)
