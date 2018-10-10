af73b2f579dcbe6976de76310fef35ec637c70e1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:03:47 -0400

    Saving plot1.R

--- plot1.R
+++ plot1.R
@@ -16,15 +16,6 @@ assault
 urbanpop<-ggplot(df, aes(df$UrbanPop))+ geom_histogram(color="black", fill="red", bins = 50)+  scale_x_continuous("Murder Rate")+ scale_y_continuous("Count")
 urbanpop
 
-#Box plot for population
-popbox<-ggplot(df, aes(x="", y=df$population, color=population))+geom_boxplot()+scale_y_continuous(name="Population", labels = comma)+
-  stat_summary(fun.ymin=median, fun.ymax=median, fun.y=median, geom="crossbar")
-popbox
-
-#Box plot for Murder Rate
-murbox<-ggplot(df, aes(x="", y=df$Murder, color=Murder))+geom_boxplot()+scale_y_continuous(name="Murder", labels = comma)+
-  stat_summary(fun.ymin=median, fun.ymax=median, fun.y=median, geom="crossbar")
-murbox
 
 df$nMurder <- as.integer(df$population*df$Murder/100000)
 
