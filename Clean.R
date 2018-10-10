be4103bdc8ddbfb588731038cf134583250b0cae
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 13:42:24 -0400

    Saving Clean.R

--- Clean.R
+++ Clean.R
@@ -1,8 +1,11 @@
 
-data<-data[,-c(1:4)]
-#renaming columns
-colnames(data)<-c('statesName','population','popover18','percentover18')
-#Deleting the unneeded rows!!
-data<-data[-c(1,10,53),]
-rownames(data)<-as.vector(data[,"statesName"])
-nrow(data)
+clean<-function(data){
+
+    data<-data[,-c(1:4)] 
+    #renaming columns
+    colnames(data)<-c('statesName','population','popover18','percentover18')
+    #Deleting the unneeded rows!!
+    data<-data[-c(1,10,53),]
+    rownames(data)<-as.vector(data[,"statesName"])
+    nrow(data)
+}
