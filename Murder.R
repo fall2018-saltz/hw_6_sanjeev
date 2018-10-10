d53a1b3b2cfe9020f67cfc08b376ecb2322a4fe2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:22:45 -0400

    Saving Murder.R

--- Murder.R
+++ Murder.R
@@ -14,7 +14,7 @@ murbar2<-ggplot(df, aes(x=stateName, y=nMurder), angle=90)+geom_bar(stat = "iden
   ggtitle("Chart with the Murder Rate per state")+
   theme(axis.text.x=element_text(angle=90))+
   labs(x="State",y="Number of Murders")
-
+murbar2
 
 #Another way to do achieve this is to rotate the whole chart 90 degrees
 murbar3<-ggplot(df, aes(x=stateName, y=nMurder))+geom_bar(stat = "identity", fill='white',color='black')+
