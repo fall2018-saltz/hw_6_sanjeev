70dcb2616db2eacb4fef0ba4689b0ab97065722e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 16:25:41 -0400

    Saving Merge.R

--- Merge.R
+++ Merge.R
@@ -4,5 +4,7 @@ arrests<-USArrests
 
 #Creating a new column in to merge the two datasets
 rownames(data)<-as.vector(data[,"stateName"])
+
+#Merging the two datasets
 df<-merge(data,arrests,by="row.names")
 str(df)
