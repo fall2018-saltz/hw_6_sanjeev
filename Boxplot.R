4567fc396ca454b616ea526580f96397df7bfc74
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:25:07 -0400

    Saving Boxplot.R

--- Boxplot.R
+++ Boxplot.R
@@ -1,6 +1,7 @@
 
+library(ggplot2)
 #Box plot for population
-popbox<-ggplot(df, aes(x="", y=df$population, color=population))+geom_boxplot()+scale_y_continuous(name="Population", labels = comma)+
+popbox<-ggplot(df, aes(x=stateName, y=df$population, color=population))+geom_boxplot()+scale_y_continuous(name="Population", labels = comma)+
   stat_summary(fun.ymin=median, fun.ymax=median, fun.y=median, geom="crossbar")
 popbox
 
