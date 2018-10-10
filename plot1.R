0e7e0dad7895b9c93e9b060e0655d9b74bf0aa94
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:34:29 -0400

    Saving plot1.R

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 library(ggplot2)
 library(scales)
 
-population<-ggplot(df, aes(df$population))+ geom_histogram(color="black", fill="white", bins = 500000)+  scale_x_continuous("Population")+ scale_y_continuous("Count")
+population<-ggplot(df, aes(df$population))+ geom_histogram(color="black", fill="white", bins = 500)+  scale_x_continuous("Population")+ scale_y_continuous("Count")
 population
 
 murder<-ggplot(df, aes(df$Murder))+ geom_histogram(color="black", fill="red", bins = 50)+  scale_x_continuous("Murder Rate")+ scale_y_continuous("Count")
