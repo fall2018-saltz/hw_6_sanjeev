6997fc27f96514378ee37d3d0b6a7a5d4e0ded68
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:31:04 -0400

    Saving plot1.R

--- plot1.R
+++ plot1.R
@@ -42,3 +42,9 @@ df$statesName<-reorder(df$statesName,df$Murder)
 ggplot(df, aes(x=statesName, y=Murder), angle=90)+geom_bar(stat = "identity", fill='white',color='black')+
   geom_text(aes(label=Murder),vjust=0.4,hjust=-1, color="red", size=3, angle=90) +
   ggtitle("Chart with the number of murders per state")+theme(axis.text.x=element_text(angle=90))
+
+#Sorted x-axis with Percentover18 as the color of the bar
+ggplot(df,aes(x=statesName, y=Murder, fill=percentovet18))+
+  geom_bar(stat = "identity")+geom_text(aes(label=Murder),vjust=0.4,hjust=-1, color="red", size=3, angle=90) +
+  ggtitle("Chart with the number of murders per state")+
+  theme(axis.text.x=element_text(angle=90))
