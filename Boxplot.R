23539c2343fa4ea944317ad6a8f819efbad82945
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:04:26 -0400

    Saving Boxplot.R

--- Boxplot.R
+++ Boxplot.R
@@ -1,1 +1,10 @@
 
+#Box plot for population
+popbox<-ggplot(df, aes(x="", y=df$population, color=population))+geom_boxplot()+scale_y_continuous(name="Population", labels = comma)+
+  stat_summary(fun.ymin=median, fun.ymax=median, fun.y=median, geom="crossbar")
+popbox
+
+#Box plot for Murder Rate
+murbox<-ggplot(df, aes(x="", y=df$Murder, color=Murder))+geom_boxplot()+scale_y_continuous(name="Murder", labels = comma)+
+  stat_summary(fun.ymin=median, fun.ymax=median, fun.y=median, geom="crossbar")
+murbox
