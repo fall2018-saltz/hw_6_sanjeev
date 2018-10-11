587125cb031cd8c32f168282bc8af3325febe628
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 16:59:20 -0400

    Saving map.R

--- map.R
+++ map.R
@@ -10,6 +10,6 @@ df$stateName<-tolower(df$stateName)
 str(df)
 map<-ggplot(df, aes(map_id=stateName))
 
-us<-map+geom_map(data=df,map=us,fill='white', color='black')+expand_limits(x=df$x,y=df$y)
+us<-map+geom_map(data=df,map=us,fill='white', color='black')+expand_limits(x=df$x,y=4-df$y)
 
 
