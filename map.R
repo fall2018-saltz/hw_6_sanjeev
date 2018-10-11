43d8a0a5042fb8720c9770cf59a1d5245d74fd65
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 17:16:10 -0400

    Saving map.R

--- map.R
+++ map.R
@@ -9,7 +9,7 @@ df$stateName<-tolower(df$stateName)
 
 str(df)
 map<-ggplot(df, aes(map_id=stateName))
-us<-map+geom_map(data=df,map=us, color='black', aes(fill=df$statearea))+
+usa<-map+geom_map(data=df,map=us, color='black', aes(fill=df$statearea))+
 expand_limits(x=us$long,y=us$lat)+
 coord_map()+ggtitle("State Area")
 
