225d128a189650273f92e16094d87632adc74788
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 17:10:22 -0400

    Saving map.R

--- map.R
+++ map.R
@@ -11,6 +11,7 @@ str(df)
 map<-ggplot(df, aes(map_id=stateName))
 
 us<-map+geom_map(data=df,map=us, color='black', aes(fill=df$statearea))+expand_limits(x=us$long,y=us$lat)
-map<-ggplot(df, aes(map_id=stateName))
-murder<-map+geom_map(data=df,map=us, color='black', aes(fill=df$Murder))+expand_limits(x=us$long,y=us$lat)
+
+murder<-ggplot(df, aes(map_id=stateName))
+murder<-murder+geom_map(data=df,map=us, color='black', aes(fill=df$Murder))+expand_limits(x=us$long,y=us$lat)
 
