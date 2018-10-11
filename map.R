8c80a330d0ffa640ff2fae28b95994c141085e2d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 17:14:43 -0400

    Saving map.R

--- map.R
+++ map.R
@@ -9,14 +9,13 @@ df$stateName<-tolower(df$stateName)
 
 str(df)
 map<-ggplot(df, aes(map_id=stateName))
-
 us<-map+geom_map(data=df,map=us, color='black', aes(fill=df$statearea))+
 expand_limits(x=us$long,y=us$lat)+
 coord_map()+ggtitle("State Area")
 
-#murder<-ggplot(df, aes(map_id=stateName))
-#murder<-murder+geom_map(data=df,map=us, color='black', aes(fill=df$Murder))+
-#expand_limits(x=us$long,y=us$lat)+
-#coord_map()+ggtitle("Murder Rate across US")
+map2<-ggplot(df, aes(map_id=stateName))
+murder<-map2+geom_map(data=df,map=us, color='black', aes(fill=df$Murder))+
+expand_limits(x=us$long,y=us$lat)+
+coord_map()+ggtitle("Murder Rate across US")
 
 
